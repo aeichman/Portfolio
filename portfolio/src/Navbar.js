cf92909c25f6f5aa93cfbb9cd975013272087563
Adam <105758382+aeichman@users.noreply.github.com>
2023-07-03 12:55:49 +0200

    adding sidebar

--- portfolio/src/Navbar.js
+++ portfolio/src/Navbar.js
@@ -1,10 +1,24 @@
 import { NavLink } from 'react-router-dom'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Cross as Hamburger } from 'hamburger-react'
 import './App.css';
 
 const Navbar = () => {
   const [isOpen, setOpen] = useState(false)
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 900) {
+        setOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
     return (
       <nav className='navbar'>
         <div className='nav-wrapper'>
@@ -17,8 +31,16 @@ const Navbar = () => {
             </ul>
           </div>
           <Hamburger className='hamburger' toggled={isOpen} toggle={setOpen} size={24} color="#fff" />
-
         </div>
+        {isOpen && window.innerWidth <= 900 && (
+        <div className="sidebar">
+          <ul>
+            <li><NavLink to="/" onClick={() => setOpen(false)}>Home</NavLink></li>
+            <li><NavLink to="/about" onClick={() => setOpen(false)}>About</NavLink></li>
+            <li><NavLink to="/projects" onClick={() => setOpen(false)}>Projects</NavLink></li>
+          </ul>
+        </div>
+        )}
       </nav>
     )
   }
