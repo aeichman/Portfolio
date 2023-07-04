7ca4361fe7330b10fc063ad96464b8512ec9d6c6
Adam <105758382+aeichman@users.noreply.github.com>
2023-07-04 11:19:55 +0200

    Sidebar Updates
    Moving Navbar css to Nav.css and page setup

--- portfolio/src/Navbar.js
+++ portfolio/src/Navbar.js
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 import { Cross as Hamburger } from 'hamburger-react'
-import './App.css';
+import './Nav.css';
 
 const Navbar = () => {
   const [isOpen, setOpen] = useState(false)
@@ -23,14 +23,7 @@ const Navbar = () => {
       <nav className='navbar'>
         <div className='nav-wrapper'>
           <div className="logoContainer"><p>LOGO</p></div>
-          {/* <div className={isOpen ? 'nav-links open' : 'nav-links'}>
-            <ul>
-              <li><NavLink to="/" onClick={() => setOpen(false)}>Home</NavLink></li>
-              <li><NavLink to="/about" onClick={() => setOpen(false)}>About</NavLink></li>
-              <li><NavLink to="/projects" onClick={() => setOpen(false)}>Projects</NavLink></li>
-            </ul>
-          </div> */}
-          <Hamburger className='hamburger' toggled={isOpen} toggle={setOpen} size={24} color="#fff" />
+          <Hamburger className='hamburger' toggled={isOpen} toggle={setOpen} size={24} color="#000" />
         </div>
         {isOpen && (
         <div className="sidebar">
@@ -38,6 +31,7 @@ const Navbar = () => {
             <li><NavLink to="/" onClick={() => setOpen(false)}>Home</NavLink></li>
             <li><NavLink to="/about" onClick={() => setOpen(false)}>About</NavLink></li>
             <li><NavLink to="/projects" onClick={() => setOpen(false)}>Projects</NavLink></li>
+            <li><NavLink to="/contact" onClick={() => setOpen(false)}>Contact</NavLink></li>
           </ul>
         </div>
         )}
