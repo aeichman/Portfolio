0069cdd24b9835c7f44781037f4f4df1b620d674
Adam <105758382+aeichman@users.noreply.github.com>
2023-07-17 15:23:38 +0200

    Adding Header Animation

--- portfolio/src/Header.js
+++ portfolio/src/Header.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { useCallback } from "react";
 import { TypeAnimation } from 'react-type-animation';
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
-
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim";
 
 
 const Header = () => {
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
     return (
         <div className='intro-wrapper'>
             <div className='intro-text'>
@@ -32,7 +43,78 @@ const Header = () => {
             </div>
             
             </div>
-            <div className='intro-graphic'></div>
+            <div className='intro-graphic'>
+                <Particles
+                    id="tsparticles"
+                    init={particlesInit}
+                    loaded={particlesLoaded}
+                    options={{
+                        fpsLimit: 120,
+                        interactivity: {
+                            events: {
+                                onClick: {
+                                    enable: true,
+                                    mode: "push",
+                                },
+                                onHover: {
+                                    enable: true,
+                                    mode: "repulse",
+                                },
+                                resize: true,
+                            },
+                            modes: {
+                                push: {
+                                    quantity: 4,
+                                },
+                                repulse: {
+                                    distance: 100,
+                                    duration: 0.4,
+                                },
+                            },
+                        },
+                        fullScreen: { enable: false },
+                        particles: {
+                            color: {
+                                value: "#ffffff",
+                            },
+                            links: {
+                                color: "#FFFF00",
+                                distance: 150,
+                                enable: true,
+                                opacity: 0.5,
+                                width: 1,
+                            },
+                            move: {
+                                direction: "none",
+                                enable: true,
+                                outModes: {
+                                    default: "bounce",
+                                },
+                                random: false,
+                                speed: 3,
+                                straight: false,
+                            },
+                            number: {
+                                density: {
+                                    enable: true,
+                                    area: 650,
+                                },
+                                value: 125,
+                            },
+                            opacity: {
+                                value: 0.5,
+                            },
+                            shape: {
+                                type: "circle",
+                            },
+                            size: {
+                                value: { min: 0.5, max: 3},
+                            },
+                        },
+                        detectRetina: true,
+                    }}
+                />
+            </div>
         </div>
       )
 }
