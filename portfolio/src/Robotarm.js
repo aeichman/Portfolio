f8e9923bfc3aadaf27b32ea8607072074697b78d
Adam <105758382+aeichman@users.noreply.github.com>
2023-07-17 15:43:12 +0200

    Project Pages Setup

---
+++ portfolio/src/Robotarm.js
@@ -0,0 +1,16 @@
+import './Projects.css';
+import { BiLogoPython, BiLogoReact, BiLogoFlask, BiLogoCPlusPlus} from "react-icons/bi";
+import { SiArduino } from "react-icons/si";
+import { AiFillGithub } from "react-icons/ai";
+
+const Robotarm = () => {
+    return (
+        <div className='project-wrapper'>
+            <div className='project-header'>
+
+            </div>
+        </div>
+      )
+}
+  
+export default Robotarm;
